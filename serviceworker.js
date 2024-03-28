b36e096051d5ad06b17c602c0a2aa68ee63bcf27
2021.sneha.utekar@ves.ac.in <2021.sneha.utekar@ves.ac.in>
2024-03-28 13:59:03 +0530

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+
+var staticCacheName = "pwa";
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => {
+                    return name !== staticCacheName;
+                }).map(name => {
+                    return caches.delete(name);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
